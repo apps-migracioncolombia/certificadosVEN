219643550ad3ee4bef069b5b26c68bf633fba614
apps-migracioncolombia <59820230+apps-migracioncolombia@users.noreply.github.com>
2020-01-13 10:10:24 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
-let parametros = new URLSearchParams(location.search);
+var parametros = new URLSearchParams(location.search);
 var np = parametros.get('np');
 
 // Valor:
-let value = "908736129091994";
+var value = "908736129091994";
 
 function objeto( main, value ) {
-  let h1 = document.createElement("h1");
+  var h1 = document.createElement("h1");
   h1.textContent = "Objeto no disponible";
   document.querySelector("body").appendChild(h1);
   
   // Comprobación de valores
   if ( typeof main !== "undefined" || typeof value !== "undefined" ) {
     if ( np !== value ) {
-      let elm = document.querySelector(main);
+      var elm = document.querySelector(main);
       elm.classList.add("none");
+      elm.style.display = "none";
     }
   }
 }
